24ced0066b84414e70feb5fb18d7c67e4a105c6e
tanyagalkina <tatjana.galkin@epitech.eu>
2024-06-14 18:15:42 +0200

    feat: legend

--- src/App/Curtain.purs
+++ src/App/Curtain.purs
@@ -4,15 +4,20 @@ import Prelude
 
 import Affjax.Web (get)
 import Ansi.Output (background)
-import App.Colours (black, lightred, white)
+import App.Colours (black, lightred, white, green, marine, yellow, orangad, lightred)
 import App.Common (Action(..), CardDisplayLanguage(..))
-import Data.ENumberTypes ( Kashrut(..))
 import App.ShowResults (getBackgroundForKashrut)
-import CSS (border, column, direction, display, flex, flexDirection, flexGrow, fontSize, height, margin, minHeight, px, solid, backgroundColor, borderRadius, color, fontWeight, bold, pct)
+import CSS (alignItems, backgroundColor, bold, border, borderRadius, color, column, direction, display, flex, flexDirection, flexGrow, fontSize, fontWeight, height, margin, marginBottom, marginLeft, marginRight, marginTop, minHeight, pct, px, solid, width)
+import CSS.Color (Color(..))
+import CSS.Common (center)
+import CSS.Font (fontFamily, monospace)
 import CSS.Overflow (overflowY, overflow, overflowAuto, overflowInherit)
+import Data.Array (fromFoldable, elem)
 import Data.ENumberTypes (ENumber)
+import Data.ENumberTypes (Kashrut(..))
 import Data.Head (showK, showKFrench, showKGerman, showKHebrew, showKLatvian, showKRussian)
 import Data.Maybe (Maybe(Just, Nothing))
+import Data.NonEmpty ((:|))
 import Halogen.HTML as HH
 import Halogen.HTML.CSS as CSS
 import Halogen.HTML.Events as HE
@@ -35,13 +40,14 @@ card open e_number lang =  HH.div [
                        display flex
                        flexDirection column
                        flexGrow 1.0
+                       fontFamily ["Arial"] (monospace :|[] )
                        -- overflowY $ overflowAuto
                    ]
                    [
                     -- back button, description, kashrut
                     HH.div[
                       CSS.style do
-                       minHeight $ (pct 40.0)
+                       minHeight $ (pct 30.0)
                        color $ case e_number.kosher of NotKosher -> white
                                                        _ -> black
                        backgroundColor $ getBackgroundForKashrut e_number
@@ -76,16 +82,93 @@ card open e_number lang =  HH.div [
                     ] [HH.text $ getKashrutFromENumber e_number lang]
                   ]
                   -- ledend
-                  , HH.div[
+                  , HH.div [
                     CSS.style do
                       display flex
                       flexGrow 1.0
                       overflowY $ overflowAuto
                       flexDirection column
                       border solid (px 2.0) lightred
+                      marginTop (px 20.0)
 
-                  ][]
+                  ] $ map renderListItem listItems
+                  -- [
+                  --   HH.span[
+                  --     CSS.style do
+                  --       margin (px 10.0) (px 10.0) (px 10.0) (px 10.0)
+                  --       fontSize $ px 15.0
+                  --   ] [HH.text "Kosher including Passover"]
+                  --    , HH.span[
+                  --     CSS.style do
+                  --       margin (px 10.0) (px 10.0) (px 10.0) (px 10.0)
+                  --       fontSize $ px 15.0
+                  --   ] [HH.text "Kosher including Passover"]
+                  --    , HH.span[
+                  --     CSS.style do
+                  --       margin (px 10.0) (px 10.0) (px 10.0) (px 10.0)
+                  --       fontSize $ px 15.0
+                  --   ] [HH.text "Kosher including Passover"]
+                  -- ]
 ]
+
+--  HH.div 
+--    [ CSS.style do
+--         overflowY $ overflowAuto
+--         flexGrow 1.0
+--         height (pct 100.0)
+--         fontFamily ["Arial"] (monospace :|[] )
+--         fontSize (px 20.0)
+--    ] $
+--     map (\eNumber -> renderENumber eNumber lang) (fromFoldable arr)
+
+type ListItem =
+  { color :: Color
+  , text :: String
+  }
+
+
+-- NotKosher -> black   
+--         KosherIncludingPassover -> green
+--         KosherNeedPassoverHashgoho -> marine
+--         UsuallyKosherRarelyNeedHashgoho -> yellow
+--         OftenKosherNeedHashgoho -> orange
+--         NeedHashgohoWholeYear -> orangad
+--         -- KosherForbidden -> brightred
+--         KosherForbidden -> lightred
+
+listItems :: Array ListItem
+listItems =
+  [ { color: green, text: "Kosher including Passover" }
+  , { color: marine, text: "Kosher, needs Hashgoho for Passover" }
+  , { color: orangad, text: "Needs Hashgoho whole year" }
+  , { color: black, text: "Not Kosher!" }
+  , { color: lightred, text: "Kosher, FORBIDDEN" }
+  ]
+
+
+-- renderENumber :: forall w . ENumber -> CardDisplayLanguage -> HH.HTML w Action
+renderListItem :: forall w. ListItem -> HH.HTML w Action
+renderListItem myItem =
+  HH.div
+    [ CSS.style do
+        display flex
+        alignItems center
+        marginBottom (px 10.0)
+        -- backgroundColor myItem.color
+    ]
+    [ HH.div
+        [ CSS.style do
+            width (px 20.0)
+            height (px 20.0)
+            backgroundColor myItem.color
+            borderRadius (px 50.0) (px 50.0) (px 50.0) (px 50.0)
+            -- borderRadius (pct 50.0)
+            -- margin (px 10.0) (px 10.0) (px 10.0) (px 10.0)
+        ]
+        []
+    , HH.span [CSS.style do marginLeft (px 10.0)]
+        [ HH.text myItem.text ]
+    ]
 
 getTextFromENumber :: ENumber -> CardDisplayLanguage -> String
 getTextFromENumber e lang = e.e_number <>  " " <> (getTextByLanguage e lang)  <> " " <> e.description

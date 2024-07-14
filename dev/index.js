819dbabab19bb1c0752c493c8850da5bf7155056
tanyagalkina <tatjana.galkin@epitech.eu>
2024-07-14 09:26:03 +0200

    feat: sw try

--- dev/index.js
+++ dev/index.js
@@ -1,2 +1,14 @@
 require("../output/Main/index.js").main();
 import "../src/App/styles.css"
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register(new URL('./service-worker.js', import.meta.url)).then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}

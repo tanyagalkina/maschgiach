ad37e4707e30bc03da327aff938aa41d0d6ca6c9
tanyagalkina <tatjana.galkin@epitech.eu>
2024-07-19 10:28:37 +0200

    feat: tuning wipe card to the left

--- src/App/Card.purs
+++ src/App/Card.purs
@@ -37,7 +37,7 @@ newCard open e_number lang =  HH.div [
 card :: forall w. Boolean -> ENumber -> CardDisplayLanguage -> HH.HTML w Action
 card open e_number lang =
   HH.div [
-                    css "fade-in"
+                    css "fade-in-card"
                      , CSS.style do
                        display flex
                        flexDirection column

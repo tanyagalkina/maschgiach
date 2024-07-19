237394bb43b9896d3794891fb811140d1f587810
tanyagalkina <tatjana.galkin@epitech.eu>
2024-07-19 11:07:04 +0200

    feat: debug wipe card

--- src/App/Card.purs
+++ src/App/Card.purs
@@ -38,7 +38,7 @@ newCard open e_number lang =  HH.div [
 card :: forall w. Boolean -> ENumber -> CardDisplayLanguage -> HH.HTML w Action
 card open e_number lang =
   HH.div [
-                    css "fade-in-card"
+                    css "fade-in"
                      , HP.id "open-card"
                      , CSS.style do
                        display flex

a6aacef76ff2adda254cc4111ce59c6aad6015d2
tanyagalkina <tatjana.galkin@epitech.eu>
2024-09-22 12:23:45 +0200

    docs: comments on 700 numbers

--- src/Data/Nb_700_799.purs
+++ src/Data/Nb_700_799.purs
@@ -2,4 +2,15 @@
 
 module Data.Nb700to799 where
 
-import Prelude
+-- E700 – E799 – Antibiotikas (netiek izmantotas pārtikas ražošanā)
+-- antibiotics: NOT used in food production
+-- E700 Bacitracīns (Bacitracin)
+-- E701 Tetraciklīns (Tetracyclines)
+-- E710 Spiramicīns (Spiramycins)
+-- E711 Virginamicīns (Virginiamicins)
+-- E712 Flavofosfolipols (Flavophospholipol)
+-- E713 Tilozīns (Tylosin)
+-- E714 Monenzīns (Monensin)
+-- E715 Avoparcīns (Avoparcin)
+-- E716 Salinomicīns (Salinomycin)
+-- E717 Avilamicīns (Avilamycin)
